a6fed8860da3f930f90f9db2762672112e3f0452
wait-light <waitlight99@gmail.com>
2022-04-29 22:49:45 +0800

    add system-start.sh

---
+++ system-start.sh
@@ -0,0 +1,9 @@
+mvn clean install
+cd ./tik-online-judge-auth && mvn clean install
+cd ../tik-online-judge-executor && mvn clean install
+cd ../tik-online-judge-gateway && mvn clean install
+cd ../tik-online-judge-message && mvn clean install
+cd ../tik-online-judge-social && mvn clean install
+cd ../tik-online-judge-thirdpart && mvn clean install
+cd ..
+compost up -d
